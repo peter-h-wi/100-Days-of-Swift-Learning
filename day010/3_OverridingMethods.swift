6a9a72d0a8661d821142a6f3bebb1255bf38b3be
Peter Wi <peter.h.wi96@gmail.com>
2021-10-05 15:52:07 -0400

    Completed Day 10

--- day010/3_OverridingMethods.swift
+++ day010/3_OverridingMethods.swift
@@ -4,3 +4,39 @@
 // Source for learning: Hacking With Swift by Paul Hudson
 
 import UIKit
+
+class Car {
+    var name: String
+    var yearProduced: Int
+
+    init(name: String, yearProduced: Int) {
+        self.name = name
+        self.yearProduced = yearProduced
+    }
+
+    func makeNoise() {
+        print("deo deo deo deo")
+    }
+}
+
+let justCar = Car("civic", 2015)
+justCar.makeNoise()
+
+class MuscleCar: Car {
+    init(name: String) {
+        super.init(name: name, yearProduced: 2020)
+    }
+
+    override func makeNoise() {
+        print("Woooooong! woong!")
+    }
+}
+
+let mustang2020 = MuscleCar("Mustang")
+mustang2020.makeNoise()
+
+
+/* [ Why to override a method ]
+You can customize the functions and stuffs.
+you can create subclasses of Apple's table and override the parts you want to change.
+*/
--- day010/6_Deinitializers.swift
+++ day010/6_Deinitializers.swift
@@ -4,3 +4,59 @@
 // Source for learning: Hacking With Swift by Paul Hudson
 
 import UIKit
+
+class Dog {
+    var name = "Robert"
+
+    init() {
+        print("\(name) is born!")
+    }
+
+    deinit {
+        print("\(name) is gone!")
+    }
+
+    func barking() {
+        print("Wooh wooh, I am \(name)")
+    }
+}
+
+for _ in 1...3 {
+    // create a object and destroyed
+    let dog = Dog()
+    dog.barking()
+}
+
+
+
+/* Output
+
+Robert is born!
+Wooh wooh, I am Robert
+Robert is gone!
+Robert is born!
+Wooh wooh, I am Robert
+Robert is gone!
+Robert is born!
+Wooh wooh, I am Robert
+Robert is gone!
+
+*/
+
+/* [ Why classes have deinitializers and structs don't? ]
+
+deinit <-> init()
+
+<struct>
+- struct don't have deinitializers => we can't tell when they are destroyed
+- but, we can create a struct inside a method and the methods ends, the struct is destroyed.
+- they don't need the deinitializers as they are just their own unique copies.
+
+<class>
+- classes have complex copying behavior that means several copies
+- so, it's hard to say if the class instance is destroyed
+    (meaning, when the final variable pointing to it has gone away.)
+- how do we know? Automatic Referencing Counting (ARC) tracks how many copies of each class instance exists
+- so, when ARC is 0, Swift call denitializer and destroy the object.
+
+*/
