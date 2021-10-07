ac792c67c1adc19c490738d85ce2892bae390dd2
Peter Wi <peter.h.wi96@gmail.com>
2021-10-07 13:50:02 -0400

    Done first four

--- day012/1_HandlingMissingData.swift
+++ day012/1_HandlingMissingData.swift
@@ -4,3 +4,12 @@
 // Source for learning: Hacking With Swift by Paul Hudson
 
 import UIKit
+
+// If you don't know someone's name, you can put "Unknown", but you need to memorize it.
+// Using optionals, you can store nil value.
+var name: String? = nil
+
+name = "Robert"
+
+// I don't know can be nil.
+var areThereAline: Bool? = nil
--- day012/2_UnwrappingOptionals.swift
+++ day012/2_UnwrappingOptionals.swift
@@ -4,3 +4,30 @@
 // Source for learning: Hacking With Swift by Paul Hudson
 
 import UIKit
+
+var ingredients: [String]? = nil
+
+// what happen if we use ingredients.count ?
+// ingredietns.count is unsafe.
+// so unwrapping is needed : look inside the optional and see what's there.
+// 1. "if let" syntax: unwraps with a condition.
+
+if let unwrapped = ingredients {
+    print("\(unwrapped.count) ingredients")
+} else {
+    print("No ingredients.")
+}
+// if ingredients hold items, it will be put inside unwrapped as a regular array
+//      then, we can read its count property inside the condition.
+// if ingredients are empty, the else code will be run.
+
+func getAge() -> Int? {
+    5
+}
+
+if let hisAge = getAge() {
+    print("He is \(hisAge) years old.")
+} else {
+    print("No information.")
+}
+// Swift will not let us use optionals without unwrapping them first.
