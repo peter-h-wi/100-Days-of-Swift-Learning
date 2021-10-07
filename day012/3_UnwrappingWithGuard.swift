f49424e172f8e3057d9a9473a67139005655e624
Peter Wi <peter.h.wi96@gmail.com>
2021-10-07 13:49:52 -0400

    Done first four

--- day012/3_UnwrappingWithGuard.swift
+++ day012/3_UnwrappingWithGuard.swift
@@ -4,3 +4,38 @@
 // Source for learning: Hacking With Swift by Paul Hudson
 
 import UIKit
+
+// another way to "if let" is "guard let"
+
+func getAnswer(_ answer: String?) {
+    guard let unwrapped = answer else {
+        print("Input invalid answer.")
+        return
+    }
+    // from here, you are safe.
+    print("Your answer is \(unwrapped)."
+    )
+}
+
+/* [ if let vs. guard let]
+guard: exit the current function, loop, or condition if the check fails.
+        ==> so any values you unwrap using it will stay around after the check.
+*/
+
+func getYearMade() -> Int? {
+    2018
+}
+
+func printYearMade() {
+    if let year = getYearMade() {
+        print(year)
+    }
+}
+
+func printYearMade2() {
+    guard let year = getYearMade() else {
+        return
+    }
+
+    print(year)
+} 
