35098393a6d5dcc67039c627a084db22228efb63
Peter Wi <peter.h.wi96@gmail.com>
2021-10-14 16:48:20 -0400

    day19 - by designing your layout

---
+++ day019-021/Project2/Project2/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Project2
+//
+//  Created by Peter Wi on 10/14/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var button1: UIButton!
+    @IBOutlet var button2: UIButton!
+    @IBOutlet var button3: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
