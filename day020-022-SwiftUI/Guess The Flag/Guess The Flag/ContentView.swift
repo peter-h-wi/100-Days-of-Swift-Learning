01b6da84a53b345eb01a6d4e173aecc1f806ae34
Peter Wi <peter.h.wi96@gmail.com>
2021-10-18 18:32:22 -0400

    on the way of image and button - day 20

--- day020-022-SwiftUI/Guess The Flag/Guess The Flag/ContentView.swift
+++ day020-022-SwiftUI/Guess The Flag/Guess The Flag/ContentView.swift
@@ -11,14 +11,27 @@ struct ContentView: View {
     var body: some View {
         
         ZStack {
-            // LinearGradient Format
-            //LinearGradient(gradient: Gradient(colors: [.white, .black]), startPoint: .top, endPoint: .bottom)
-
-            // RadialGradient Format
-            //RadialGradient(gradient: Gradient(colors: [.blue, .black]), center: .center, startRadius: 20, endRadius: 200)
+            // Both do the same
+            Button("Tap me!") {
+                print("Button was tapped")
+            }
             
-            AngularGradient(gradient: Gradient(colors: [.red, .yellow, .green, .blue, .purple, .red]), center: .center)
-            Text("Your content")
+            Button(action: {
+                print("Button was tapped")
+            }) {
+                Text("Tap me!")
+            }
+            
+            // it will load an image called "Pencil" that you have added to your project.
+            Image("pencil")
+            
+            // it will load the same image, but will not read it out for users who have enabled the screen reader. This is useful for images that don't convey additional important information.
+            Image(decorative: "pencil")
+            
+            // it will load the pencil icon that is built into iOS. This uses Apple's SF symbols icon collection, and you can search for icons you like - download Apple's free SF Symbols app from the web to see the full set.
+            Image(systemName: "pencil")
+            
+            // By default the screen reader will read your image name if it is enabled, so make sure you give your images clear names if you want to avoid confusing the user. Or, if they don't actually add information that isn't already elsewhere on the screen, use the Image(decorative:) initializer.
         }
         
     }
