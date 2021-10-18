249a3d61bc008a7d50364b2ebe2ab190ce9366e6
Peter Wi <peter.h.wi96@gmail.com>
2021-10-18 18:20:06 -0400

    Color, and safe area - Day 20

--- day020-022-SwiftUI/Guess The Flag/Guess The Flag/ContentView.swift
+++ day020-022-SwiftUI/Guess The Flag/Guess The Flag/ContentView.swift
@@ -9,32 +9,16 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        // Vertical stack of views
-        VStack(spacing: 20) {
-            // spacing: by default there is little or no spacing
-            // alignment: by default its views are centered
-            // it can have a maximum of 10 children
-            
-            Text("Hello, world!")
-                .padding()
-            Text("This is inside a stack")
-            Spacer()
-        }
-        
-        // Horizontal Stack
-        HStack(spacing: 20) {
-            Text("Hello, world!")
-                .padding()
-            Text("This is inside a stack")
-        }
-        
-        // Arranging things by depth
         ZStack {
-            // It doesn't have spacing, because the views overlap.
-            // but it has alignment such as (.top) --> from top to bottom, back to front
-            Text("Hello, world!")
-                .padding()
-            Text("This is inside a stack")
+            // Color.red is a view in its own right, which is why it can be used like shapes and text.
+            Color.red.frame(width: 200, height: 200)
+            // you can customize the color
+            Color(red: 1, green: 0.6, blue: 0.4).frame(width:100, height: 100)
+            
+            // you can ignore the safe area
+            // if your content is just decorative - like our background color here - then extending it outside the safe area is OK.
+            Color.blue.edgesIgnoringSafeArea(.all)
+            Text("Your content")
         }
         
     }
