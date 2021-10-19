35882f1adb15916f35ff6c95f57b4711207912d2
Peter Wi <peter.h.wi96@gmail.com>
2021-10-19 12:03:09 -0400

    Day 21 - finished

--- day020-022-SwiftUI/Guess The Flag/Guess The Flag/ContentView.swift
+++ day020-022-SwiftUI/Guess The Flag/Guess The Flag/ContentView.swift
@@ -8,16 +8,65 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var showingAlert = false
+    @State private var showingScore = false
+    @State private var scoreTitle = ""
+    
+    @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"].shuffled()
+    @State private var correctAnswer = Int.random(in: 0...2)
+    
     
     var body: some View {
-        Button("Show Alert") {
-            self.showingAlert = true
+        ZStack {
+            LinearGradient(gradient: Gradient(colors: [.blue, .black]), startPoint: .top, endPoint: .bottom)
+                .edgesIgnoringSafeArea(.all)
+            VStack(spacing: 30) {
+                VStack {
+                    Text("Tap the flag of")
+                        .foregroundColor(.white)
+                    Text(countries[correctAnswer])
+                        .foregroundColor(.white)
+                    // .largeTitle is the largest built-in font size - absed on user setting for their font size
+                        .font(.largeTitle)
+                        .fontWeight(.black)
+                }
+                
+                ForEach(0 ..< 3) { number in
+                    Button(action: {
+                        self.flagTapped(number)
+                    }) {
+                        Image(self.countries[number])
+                            .renderingMode(.original)
+                        // clipShape: rectangle, rounded rectangle, circle, and capsule
+                            .clipShape(Capsule())
+                        // overlay: drawing a border around the image -> a capture has a black stroke around its edge
+                            .overlay(Capsule().stroke(Color.black, lineWidth: 1))
+                            .shadow(color: .black, radius: 2)
+                    }
+                }
+                Spacer()
+            }
+            .alert(isPresented: $showingScore) {
+                Alert(title: Text(scoreTitle), message: Text("Your score is ???"), dismissButton: .default(Text("Continue")) {
+                    self.askQuestion()
+                })
+            }
         }
-        // $showingAlert is two-way data binding, because SwiftUI will automatically set showingAlert back to false when the alert is dismissed.
-        .alert(isPresented: $showingAlert) {
-            Alert(title: Text("Hello SwiftUI!"), message: Text("This is some detail message"), dismissButton: .default(Text("OK")))
+        
+    }
+    
+    func flagTapped(_ number: Int) {
+        if number == correctAnswer {
+            scoreTitle = "Correct"
+        } else {
+            scoreTitle = "Wrong"
         }
+        
+        showingScore = true
+    }
+    
+    func askQuestion() {
+        countries.shuffle()
+        correctAnswer = Int.random(in: 0...2)
     }
 }
 
