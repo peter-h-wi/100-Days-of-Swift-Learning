348897dad6fdfce7b337b0c2612d914b0d061374
Peter Wi <peter.h.wi96@gmail.com>
2021-10-19 15:35:50 -0400

    before some view - day 23

--- day023-024-SwiftUI/ViewAndModifier/ViewAndModifier/ContentView.swift
+++ day023-024-SwiftUI/ViewAndModifier/ViewAndModifier/ContentView.swift
@@ -23,15 +23,45 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
-        // it doesn't make the whole background as red, because for SwiftUI developers, there is nothing behind our view.
-        // -->.background(Color.red)
-        // actually there is something behind our content view called a "UIHostingController": which is the bridge between UIKit and SwiftUI. --> but if you modify, it will not work on. so donjust regard that there is nothing behind our view.
-        // thus, what you can do is using frame() modifier.
-            .frame(maxWidth: .infinity, maxHeight: .infinity)
+        VStack {
+            Text("Hello, world!")
+                .padding()
+            // it doesn't make the whole background as red, because for SwiftUI developers, there is nothing behind our view.
+            // -->.background(Color.red)
+            // actually there is something behind our content view called a "UIHostingController": which is the bridge between UIKit and SwiftUI. --> but if you modify, it will not work on. so donjust regard that there is nothing behind our view.
+            // thus, what you can do is using frame() modifier.
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                .background(Color.blue)
+                .edgesIgnoringSafeArea(.all)
+            /* modifier order matters
+                .background(Color.red)
+                .frame(width: 200, height: 200)
+             --> You will not see 200x200 red button with "Hello World" in the middle
+             --> Instead, you will see 200x200 empty square, with "Hello World" in the middle with a red rectangle directly around "Hello World"
+             */
+            
+            Button("Hello SwiftUI") {
+                print(type(of: self.body))
+            }
             .background(Color.red)
-            .edgesIgnoringSafeArea(.all)
+            .frame(width: 200, height: 200)
+            // Whenever you add motifier, it stacks new ModifiedContent up.
+            // --> the order of your modifiers matter.
+            
+            Text("Hello World")
+                .padding()
+                .background(Color.red)
+                .padding()
+                .background(Color.blue)
+                .padding()
+                .background(Color.green)
+                .padding()
+                .background(Color.yellow)
+        }
+        
+        
+        
+        
     }
 }
 
