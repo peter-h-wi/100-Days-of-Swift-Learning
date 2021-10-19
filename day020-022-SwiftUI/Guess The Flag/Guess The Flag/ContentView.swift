513816f03aed714a647959fcc014a0c634e98e38
Peter Wi <peter.h.wi96@gmail.com>
2021-10-19 11:27:17 -0400

    show alert - day 20 fin

--- day020-022-SwiftUI/Guess The Flag/Guess The Flag/ContentView.swift
+++ day020-022-SwiftUI/Guess The Flag/Guess The Flag/ContentView.swift
@@ -8,42 +8,16 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var showingAlert = false
+    
     var body: some View {
-        
-        VStack {
-            // Both do the same
-            Button("Tap me!") {
-                print("Button was tapped")
-            }
-            
-            Button(action: {
-                print("Button was tapped")
-            }) {
-                Text("Tap me!")
-            }
-            
-            // it will load an image called "Pencil" that you have added to your project.
-            Image("pencil")
-            
-            // it will load the same image, but will not read it out for users who have enabled the screen reader. This is useful for images that don't convey additional important information.
-            Image(decorative: "pencil")
-            
-            // it will load the pencil icon that is built into iOS. This uses Apple's SF symbols icon collection, and you can search for icons you like - download Apple's free SF Symbols app from the web to see the full set.
-            Image(systemName: "pencil")
-            
-            // By default the screen reader will read your image name if it is enabled, so make sure you give your images clear names if you want to avoid confusing the user. Or, if they don't actually add information that isn't already elsewhere on the screen, use the Image(decorative:) initializer.
-            
-            Button(action: {
-                print("Edit button was tapped")
-            }) {
-                HStack(spacing: 10) {
-                    Image(systemName: "pencil")
-                        .renderingMode(.original)
-                    Text("Edit hihi")
-                }
-            }
+        Button("Show Alert") {
+            self.showingAlert = true
         }
-        
+        // $showingAlert is two-way data binding, because SwiftUI will automatically set showingAlert back to false when the alert is dismissed.
+        .alert(isPresented: $showingAlert) {
+            Alert(title: Text("Hello SwiftUI!"), message: Text("This is some detail message"), dismissButton: .default(Text("OK")))
+        }
     }
 }
 
