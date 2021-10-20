730e9a4ae15330d15ca4796c4b2f6c7928a09a83
Peter Wi <peter.h.wi96@gmail.com>
2021-10-20 16:32:59 -0400

    custom modifiers - day 23

--- day023-024-SwiftUI/ViewAndModifier/ViewAndModifier/ContentView.swift
+++ day023-024-SwiftUI/ViewAndModifier/ViewAndModifier/ContentView.swift
@@ -12,39 +12,58 @@ struct ContentView: View {
     var body: some View {
         VStack(spacing: 10){
             // SWiftUI lets us split up one large view into multiple smaller views, and SwiftUI takes care of reassembling them for us.
-            Text("Yo")
-                .font(.largeTitle)
-                .padding()
-                .foregroundColor(.white)
-                .background(Color.blue)
-                .clipShape(Capsule())
-            Text("Sup")
-                .font(.largeTitle)
-                .padding()
-                .foregroundColor(.white)
-                .background(Color.blue)
-                .clipShape(Capsule())
-            // this two are identical, so we can wrap them up in a new custom view, like this:
-            CapsuleText(text: "Yo")
-            // we can also store some modifiers in the view and customize others when we use them.
-                .foregroundColor(.white)
-            CapsuleText(text: "Sup")
+            Text("Custom Modifier")
+                .modifier(Title())
+            
+            Text("Added extension")
+                .titleStyle()
+            Color.blue
+                .frame(width: 300, height: 200)
+                .watermarked(with: "Peter's SwiftUI")
         }
     }
 }
 
-struct CapsuleText: View {
-    var text: String
-    
-    var body: some View {
-        Text(text)
+// you can create custom modifiers that do something specific.
+// 1. create a custom ViewModifier struct
+struct Title: ViewModifier {
+    func body(content: Content) -> some View {
+        content
             .font(.largeTitle)
+            .foregroundColor(.white)
             .padding()
-            //.foregroundColor(.white)
             .background(Color.blue)
-            .clipShape(Capsule())
     }
 }
+
+// 2. when working with custom modifiers, it's a good idea to create extensions on View that make them easier to use.
+extension View {
+    func titleStyle() -> some View {
+        self.modifier(Title())
+    }
+}
+
+// 3. custom modifiers can create new view structure, as needed.
+struct Watermark: ViewModifier {
+    var text: String
+    
+    func body(content: Content) -> some View {
+        ZStack(alignment: .bottomTrailing) {
+            content
+            Text(text)
+                .font(.caption)
+                .foregroundColor(.white)
+                .padding(5)
+                .background(Color.black)
+        }
+    }
+}
+extension View {
+    func watermarked(with text: String) -> some View {
+        self.modifier(Watermark(text: text))
+    }
+}
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
