470c3b5ea83420468aed4f8797d21a8985fd9a4b
Peter Wi <peter.h.wi96@gmail.com>
2021-10-20 17:12:40 -0400

    custom container - day 26 finished.

--- day023-024-SwiftUI/ViewAndModifier/ViewAndModifier/ContentView.swift
+++ day023-024-SwiftUI/ViewAndModifier/ViewAndModifier/ContentView.swift
@@ -10,59 +10,62 @@ import SwiftUI
 struct ContentView: View {
     
     var body: some View {
-        VStack(spacing: 10){
-            // SWiftUI lets us split up one large view into multiple smaller views, and SwiftUI takes care of reassembling them for us.
-            Text("Custom Modifier")
-                .modifier(Title())
-            
-            Text("Added extension")
-                .titleStyle()
-            Color.blue
-                .frame(width: 300, height: 200)
-                .watermarked(with: "Peter's SwiftUI")
+        
+        GridStack(rows: 4, columns: 4) { row, col in
+            Text("R\(row) C\(col)")
         }
+        
+        GridStack(rows: 4, columns: 4) { row, col in
+            HStack {
+                Image(systemName: "\(row * 4 + col).circle")
+                Text("R\(row) C\(col)")
+            }
+        }
+        
+        // through builder, then SwiftUI will now automatically create an implicit horizontal stack inside our cell closure.
+        GridStack(rows: 4, columns: 4) {row, col in
+            Image(systemName: "\(row * 4 + col).circle")
+            Text("R\(row) C\(col)")
+        }
+        
     }
 }
 
-// you can create custom modifiers that do something specific.
-// 1. create a custom ViewModifier struct
-struct Title: ViewModifier {
-    func body(content: Content) -> some View {
-        content
-            .font(.largeTitle)
-            .foregroundColor(.white)
-            .padding()
-            .background(Color.blue)
-    }
-}
-
-// 2. when working with custom modifiers, it's a good idea to create extensions on View that make them easier to use.
-extension View {
-    func titleStyle() -> some View {
-        self.modifier(Title())
-    }
-}
-
-// 3. custom modifiers can create new view structure, as needed.
-struct Watermark: ViewModifier {
-    var text: String
+// you can create custom containers in your SwiftUI apps
+// first line uses a more advanced feature of Swift called generics, which in this case means "you can provide any kind of content you like, but whatever it is it must conform to the View protocol.
+// After the colon we repeat View again to say that GridStack itself also conforms to the View protocol.
+struct GridStack<Content: View>: View {
+    let rows: Int
+    let columns: Int
+    // let content line defines a closure that must be able to accept two integers and return some sort of content we can show
+    let content: (Int, Int) -> Content
     
-    func body(content: Content) -> some View {
-        ZStack(alignment: .bottomTrailing) {
-            content
-            Text(text)
-                .font(.caption)
-                .foregroundColor(.white)
-                .padding(5)
-                .background(Color.black)
+    var body: some View {
+        VStack {
+            ForEach(0..<rows, id: \.self) {row in
+                HStack {
+                    ForEach(0..<self.columns, id: \.self) { column in
+                        self.content(row, column)
+                    }
+                }
+            }
         }
     }
-}
-extension View {
-    func watermarked(with text: String) -> some View {
-        self.modifier(Watermark(text: text))
+    
+    // builders: allows us to send in several views and have it form an implicit stack for us.
+    // @escaping, allows us to store closures away to be used later on.
+    // similar to constructor, i think.
+    init(rows: Int, columns: Int, @ViewBuilder content: @escaping (Int, Int) -> Content) {
+        self.rows = rows
+        self.columns = columns
+        self.content = content
     }
+
 }
+
+
+
+
 
 
 struct ContentView_Previews: PreviewProvider {
