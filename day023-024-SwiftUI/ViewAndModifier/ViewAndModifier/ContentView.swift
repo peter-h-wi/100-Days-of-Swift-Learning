1e29468576c05face50b8e7fd0fa3668f77dea14
Peter Wi <peter.h.wi96@gmail.com>
2021-10-20 16:24:54 -0400

    view Composition - day 23

--- day023-024-SwiftUI/ViewAndModifier/ViewAndModifier/ContentView.swift
+++ day023-024-SwiftUI/ViewAndModifier/ViewAndModifier/ContentView.swift
@@ -8,20 +8,41 @@
 import SwiftUI
 
 struct ContentView: View {
-    // we can create two text views as properties.
-    let msg1 = Text("Hi")
-    let msg2 = Text("How are you")
-    // you can create computed properties if you want, like this:
-    var msg3: some View { Text("Doing?") }
     
     var body: some View {
-        VStack {
-            msg1
-            // you can even apply modifiers
-                .foregroundColor(.red)
-            msg2
-                .foregroundColor(.blue)
+        VStack(spacing: 10){
+            // SWiftUI lets us split up one large view into multiple smaller views, and SwiftUI takes care of reassembling them for us.
+            Text("Yo")
+                .font(.largeTitle)
+                .padding()
+                .foregroundColor(.white)
+                .background(Color.blue)
+                .clipShape(Capsule())
+            Text("Sup")
+                .font(.largeTitle)
+                .padding()
+                .foregroundColor(.white)
+                .background(Color.blue)
+                .clipShape(Capsule())
+            // this two are identical, so we can wrap them up in a new custom view, like this:
+            CapsuleText(text: "Yo")
+            // we can also store some modifiers in the view and customize others when we use them.
+                .foregroundColor(.white)
+            CapsuleText(text: "Sup")
         }
+    }
+}
+
+struct CapsuleText: View {
+    var text: String
+    
+    var body: some View {
+        Text(text)
+            .font(.largeTitle)
+            .padding()
+            //.foregroundColor(.white)
+            .background(Color.blue)
+            .clipShape(Capsule())
     }
 }
 
