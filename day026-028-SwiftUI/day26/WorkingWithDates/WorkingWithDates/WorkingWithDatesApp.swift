fb682f00683bc56cc15ecbfb866041901a011deb
Peter Wi <peter.h.wi96@gmail.com>
2021-11-02 15:56:53 -0400

    Day 26 - handling dates

---
+++ day026-028-SwiftUI/day26/WorkingWithDates/WorkingWithDates/WorkingWithDatesApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorkingWithDatesApp.swift
+//  WorkingWithDates
+//
+//  Created by Peter Wi on 11/2/21.
+//
+
+import SwiftUI
+
+@main
+struct WorkingWithDatesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
