7394a6979c3d1a0f91d00717deb315ce320aea55
Peter Wi <peter.h.wi96@gmail.com>
2021-11-12 01:15:15 +0900

    Finished day 27

--- day026-028-SwiftUI/BetterRest/BetterRest/ContentView.swift
+++ day026-028-SwiftUI/BetterRest/BetterRest/ContentView.swift
@@ -5,61 +5,99 @@
 //  Created by Peter Wi on 11/2/21.
 //
 
+// you need to import CoreML to use ML file we trained.
+// keep your imports in alphabetical order --> easier to check.
+import CoreML
 import SwiftUI
 
 struct ContentView: View {
-    @State private var sleepAmount = 8.0
-    @State private var wakeUp = Date.now
     
+    // @State private var wakeUp = Date.now
+    @State private var wakeUp = defaultWakeTime
+    @State private var sleepAmount = 8.0
+    @State private var coffeeAmount = 1
+    
+    @State private var alertTitle = ""
+    @State private var alertMessage = ""
+    @State private var showingAlert = false
+
+    // static: it belongs to the ContentView struct itself rather than a single instance of that struct.
+    static var defaultWakeTime: Date {
+        var components = DateComponents()
+        components.hour = 7
+        components.minute = 0
+        return Calendar.current.date(from: components) ?? Date.now
+    }
     
     var body: some View {
-        Form {
-            // By default, steppers are limited only bt the range of their storage.
-            // by using "in" range, we can limit the values
-            // "step": how far to move the value each time
-            Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
-            
-            // labelsHidden: remove the label
-            // displayComponents: what to show
-            // in: limit the range
-            DatePicker("Please enter a date", selection: $wakeUp, in: Date.now...)
-            
-            // 4. how do we format parameter?
-            // day - month - year is not arranging. iOS will automatically format that data using the users' preferences
-            Text(Date.now, format: .dateTime.day().month().year())
-            
-            // 5. alternatively. you can use 'formatted'.
-            Text(Date.now.formatted(date: .long, time: .omitted))
+        NavigationView {
+            Form {
+                VStack(alignment: .leading, spacing: 0) {
+                    Text("When do you want to wake up?")
+                                        .font(.headline)
+                    DatePicker("Please enter a time", selection: $wakeUp, displayedComponents: .hourAndMinute)
+                                        .labelsHidden()
+                }
+                
+                VStack(alignment: .leading, spacing: 0) {
+                    Text("Desired amount of sleep")
+                        .font(.headline)
+                    
+                    Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
+                }
+                
+                VStack(alignment: .leading, spacing: 0) {
+                    Text("Daily coffee intake")
+                        .font(.headline)
+                    
+                    Stepper(coffeeAmount == 1 ? "1 cup" : "\(coffeeAmount) cups", value: $coffeeAmount, in: 1...20)
+                }
+                
+            }
+            .navigationTitle("BetterRest")
+            // toolbar: modifier to add a trailing button to the navigation view
+            .toolbar {
+                Button("Calculate", action: calculateBedtime)
+            }
+            // alert: modifier to show alertTitle and alertMessage when shoingAlert becomes true.
+            .alert(alertTitle, isPresented: $showingAlert) {
+                Button("OK") { }
+            } message: {
+                Text(alertMessage)
+            }
         }
     }
     
-    /* Handling Date and Hours...
-    func trivialExample() {
-        /* 1: Track the seconds
-         problem: the calander not always follow 24 hr (eg. summer time)
-         solution: use iOS framework
-         let now = Date.now
-         let tomorrow = Date.now.addingTimeInterval(86400)
-         let range = now...tomorrow
-        */
+    func calculateBedtime() {
+        // do/catch: using Core ML can throw errors
+        // 1. loading the model
+        // 2. when we ask predictions
+        do {
+            // model instance: thing that reads in all our data and will output a prediction.
+            // configuration: in case you need to enable a handful of what are fairly obscure options.
+            let config = MLModelConfiguration()
+            // rename the .mlmodel to SleepCalculator --> class name will be SleepCalculator
+            let model = try SleepCalculator(configuration: config)
+            
+            let components = Calendar.current.dateComponents([.hour, .minute], from: wakeUp)
+            let hour = (components.hour ?? 0) * 60 * 60 // in second --> unit of wake
+            let minute = (components.minute ?? 0) * 60 // in second
+            
+            // the prediction value is some number in seconds. --> convert that into the time
+            let prediction = try model.prediction(wake: Double(hour + minute), estimatedSleep: sleepAmount, coffee: Double(coffeeAmount))
+            
+            // thus, when you wake up - sleeping seconds --> when to sleep
+            let sleepTime = wakeUp - prediction.actualSleep
+            alertTitle = "Your ideal bedtime is..."
+            alertMessage = sleepTime.formatted(date: .omitted, time: .shortened)
+        } catch {
+            // when something went wrong.
+            alertTitle = "Error"
+            alertMessage = "Sorry, there was a problem calculating your bedtime."
+        }
         
-        /* 2
-         DateComponents: let us read/write specific parts of a date rather than the whole thing.
-         
-         var components = DateComponents()
-         components.hour = 8
-         components.minute = 0
-         let date = Calendar.current.date(from: components) ?? Date.now
-         --> how do we pull out just the hour and mininute?
-        */
-        
-        /* 3 pull out the hour and minute
-         let components = Calendar.current.dateComponents([.hour, .minute], from: Date.now)
-         let hour = components.hour ?? 0
-         let minute = components.minute ?? 0
-        */
+        showingAlert = true
     }
-     */
     
 }
 
---
+++ day026-028-SwiftUI/day26/TrainingAModelWithCreateML/TrainingAModelWithCreateML/TrainingAModelWithCreateMLApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrainingAModelWithCreateMLApp.swift
+//  TrainingAModelWithCreateML
+//
+//  Created by peter wi on 11/11/21.
+//
+
+import SwiftUI
+
+@main
+struct TrainingAModelWithCreateMLApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
