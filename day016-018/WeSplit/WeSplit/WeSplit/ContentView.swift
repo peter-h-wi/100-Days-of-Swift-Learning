b950b6088f68e9de195bfbf784a129a6099f47ce
Peter Wi <peter.h.wi96@gmail.com>
2021-11-12 14:11:33 +0900

    add screenshot

--- day016-018/WeSplit/WeSplit/WeSplit/ContentView.swift
+++ day016-018/WeSplit/WeSplit/WeSplit/ContentView.swift
@@ -8,7 +8,7 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var checkAmount = 0.0
+    @State private var checkAmount = 20.0
     @State private var numberOfPeople = 2
     @State private var tipPercentage = 20
     
