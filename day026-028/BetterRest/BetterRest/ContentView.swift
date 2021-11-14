8b7781fc7f8b5a4622af0ac581354a68839b591e
Peter Wi <peter.h.wi96@gmail.com>
2021-11-14 23:18:26 +0900

    add screenshots and modified some UI

--- day026-028/BetterRest/BetterRest/ContentView.swift
+++ day026-028/BetterRest/BetterRest/ContentView.swift
@@ -17,61 +17,14 @@ struct ContentView: View {
     @State private var sleepAmount = 8.0
     @State private var coffeeAmount = 1
     
-    var title: String {
-        // do/catch: using Core ML can throw errors
-        // 1. loading the model
-        // 2. when we ask predictions
-        do {
-            // model instance: thing that reads in all our data and will output a prediction.
-            // configuration: in case you need to enable a handful of what are fairly obscure options.
-            let config = MLModelConfiguration()
-            // rename the .mlmodel to SleepCalculator --> class name will be SleepCalculator
-            let model = try SleepCalculator(configuration: config)
-            
-            let components = Calendar.current.dateComponents([.hour, .minute], from: wakeUp)
-            let hour = (components.hour ?? 0) * 60 * 60 // in second --> unit of wake
-            let minute = (components.minute ?? 0) * 60 // in second
-            
-            // the prediction value is some number in seconds. --> convert that into the time
-            let prediction = try model.prediction(wake: Double(hour + minute), estimatedSleep: sleepAmount, coffee: Double(coffeeAmount))
-            
-            // thus, when you wake up - sleeping seconds --> when to sleep
-            return "Your ideal bedtime is..."
-        } catch {
-            // when something went wrong.
-            return "Error"
+    var getBedtime: String {
+        get {
+            return calculateBedtime();
         }
     }
     
-    var message: String {
-        // do/catch: using Core ML can throw errors
-        // 1. loading the model
-        // 2. when we ask predictions
-        do {
-            // model instance: thing that reads in all our data and will output a prediction.
-            // configuration: in case you need to enable a handful of what are fairly obscure options.
-            let config = MLModelConfiguration()
-            // rename the .mlmodel to SleepCalculator --> class name will be SleepCalculator
-            let model = try SleepCalculator(configuration: config)
-            
-            let components = Calendar.current.dateComponents([.hour, .minute], from: wakeUp)
-            let hour = (components.hour ?? 0) * 60 * 60 // in second --> unit of wake
-            let minute = (components.minute ?? 0) * 60 // in second
-            
-            // the prediction value is some number in seconds. --> convert that into the time
-            let prediction = try model.prediction(wake: Double(hour + minute), estimatedSleep: sleepAmount, coffee: Double(coffeeAmount))
-            
-            // thus, when you wake up - sleeping seconds --> when to sleep
-            let sleepTime = wakeUp - prediction.actualSleep
-            return sleepTime.formatted(date: .omitted, time: .shortened)
-        } catch {
-            // when something went wrong.
-            return "Sorry, there was a problem calculating your bedtime."
-        }
-    }
-    
-
     // static: it belongs to the ContentView struct itself rather than a single instance of that struct.
+    
     static var defaultWakeTime: Date {
         var components = DateComponents()
         components.hour = 7
@@ -83,16 +36,31 @@ struct ContentView: View {
         NavigationView {
             Form {
                 Section {
+                    Text(getBedtime)
+                        .font(.title)
+                        .foregroundColor(.cyan)
+                } header: {
+                    Text("Your ideal beadtime is...")
+                        .font(.headline)
+                        .foregroundColor(.primary)
+                }
+                
+                Section {
                     DatePicker("Please enter a time", selection: $wakeUp, displayedComponents: .hourAndMinute)
                                         .labelsHidden()
+                                        .datePickerStyle(.wheel)
                 } header: {
                     Text("When do you want to wake up?")
+                        .font(.headline)
+                        .foregroundColor(.primary)
                 }
                 
                 Section {
                     Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
                 } header: {
                     Text("Desired amount of sleep")
+                        .font(.headline)
+                        .foregroundColor(.primary)
                 }
                 
                 Section {
@@ -101,16 +69,11 @@ struct ContentView: View {
                             Text($0 == 1 ? "1 cup" : "\($0) cups")
                         }
                     }
-                    .pickerStyle(.wheel)
+                    .pickerStyle(.menu)
                 } header: {
                     Text("Daily coffee intake")
-                }
-                                
-                Section {
-                    Text("\(message)")
-                        .font(.largeTitle)
-                } header: {
-                    Text("\(title)")
+                        .font(.headline)
+                        .foregroundColor(.primary)
                 }
             }
             .navigationTitle("BetterRest")
@@ -126,6 +89,33 @@ struct ContentView: View {
                 Text(alertMessage)
             }
              */
+        }
+    }
+    
+    func calculateBedtime() -> String {
+        // do/catch: using Core ML can throw errors
+        // 1. loading the model
+        // 2. when we ask predictions
+        do {
+            // model instance: thing that reads in all our data and will output a prediction.
+            // configuration: in case you need to enable a handful of what are fairly obscure options.
+            let config = MLModelConfiguration()
+            // rename the .mlmodel to SleepCalculator --> class name will be SleepCalculator
+            let model = try SleepCalculator(configuration: config)
+
+            let components = Calendar.current.dateComponents([.hour, .minute], from: wakeUp)
+            let hour = (components.hour ?? 0) * 60 * 60 // in second --> unit of wake
+            let minute = (components.minute ?? 0) * 60 // in second
+
+            // the prediction value is some number in seconds. --> convert that into the time
+            let prediction = try model.prediction(wake: Double(hour + minute), estimatedSleep: sleepAmount, coffee: Double(coffeeAmount))
+
+            // thus, when you wake up - sleeping seconds --> when to sleep
+            let sleepTime = wakeUp - prediction.actualSleep
+            return sleepTime.formatted(date: .omitted, time: .shortened)
+        } catch {
+            // when something went wrong.
+            return "Sorry, there was a problem calculating your bedtime."
         }
     }
 }
