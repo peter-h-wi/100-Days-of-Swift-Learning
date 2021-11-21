c918df6db82e77557dd932ae4179c3fe2908bfcf
Peter Wi <peter.h.wi96@gmail.com>
2021-11-21 17:23:55 +0900

    day 30 - word scramble

---
+++ day029-031/WordScramble/WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by peter wi on 11/21/21.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
